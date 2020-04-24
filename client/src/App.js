4e83680b5a231f1a6ec29a3d9a2f9edbfadfb7bf
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-04-24 11:19:20 -0500

    added login, bubbles and colorlist

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
-
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import PrivateRoute from './components/PrivateRoute';
 import BubblePage from './components/BubblePage';
 import Login from "./components/Login";
-// import "./styles.scss";
-// import './app.scss';
+import "./styles.scss";
 
 function App() {
   return (
@@ -17,9 +14,10 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
-        <PrivateRoute path="/bubble-page" component={BubblePage} />
       </div>
+      <PrivateRoute path="/bubble-page" component={BubblePage} />
     </Router>
   );
 }
+
 export default App;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import { axiosWithAuth } from '../utility/axiosWithAuth';
 
 const Login = (props) => {
-	const [userInput, setUserInput] = useState({ username: '', password: ''});
+    // make a post request to retrieve a token from the api
+  // when you have handled the token, navigate to the BubblePage route
+  const [userInput, setUserInput] = useState({ username: '', password: ''});
 
 	const handleChange = e => {
 		setUserInput({...userInput, [e.target.name]: e.target.value});
+		console.log(userInput);
 	}
-
 	const handleSubmit = e => {
 		e.preventDefault();
 
@@ -23,22 +25,20 @@ const Login = (props) => {
 		})
 
 	}
-
-	// make a post request to retrieve a token from the api
+// make a post request to retrieve a token from the api
 	// when you have handled the token, navigate to the BubblePage route
 	return (
-		<div className="LoginForm">
-			<h2>Login to view colors</h2>
+		<div>
 			<form onSubmit={handleSubmit}>
-				<input type="text" name="username" placeholder="Username"
+				<input type="text" name="username"
 				value={userInput.username} onChange={handleChange} />
-				<input type="password" name="password" placeholder="Password"
+				<input type="password" name="password"
 				value={userInput.password} onChange={handleChange} />
+				<input type="submit" />
 				<button>Login</button>
 			</form>
 
 		</div>
 	);
 };
-
 export default Login;
