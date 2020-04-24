17646c0102a514dee94447782b93a420a4f93ce0
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-04-24 10:42:44 -0500

    added routes and utils

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,20 +1,31 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utility/axiosWithAuth';
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
-const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+const BubblePage = (props) => {
+	const [colorList, setColorList] = useState([]);
+	// fetch your colors data from the server when the component mounts
+	// set that data to the colorList state property
 
-  return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
-  );
+  useEffect(() => {
+		axiosWithAuth().get('/api/colors')
+		.then(res => {
+			setColorList(res.data)
+		})
+		.catch(err => {
+			console.log(err);
+			alert("Error retrieving colors", props)
+		})
+	}, [props])
+
+	return (
+		<div className="BubblePageStyle">
+			<ColorList props={props} colors={colorList} updateColors={setColorList} />
+			<Bubbles colors={colorList} />
+		</div>
+	);
 };
 
 export default BubblePage;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from '../utility/axiosWithAuth';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+const Login = (props) => {
+	const [userInput, setUserInput] = useState({ username: '', password: ''});
+
+	const handleChange = e => {
+		setUserInput({...userInput, [e.target.name]: e.target.value});
+	}
+
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		axiosWithAuth().post('/api/login', userInput)
+		.then(res => {
+			console.log(res);
+			localStorage.setItem('token', res.data.payload);
+			props.history.push('/bubble-page');
+		})
+		.catch(err => {
+			console.log(err);
+			alert('Incorrect Login');
+		})
+
+	}
+
+	// make a post request to retrieve a token from the api
+	// when you have handled the token, navigate to the BubblePage route
+	return (
+		<div className="LoginForm">
+			<h2>Login to view colors</h2>
+			<form onSubmit={handleSubmit}>
+				<input type="text" name="username" placeholder="Username"
+				value={userInput.username} onChange={handleChange} />
+				<input type="password" name="password" placeholder="Password"
+				value={userInput.password} onChange={handleChange} />
+				<button>Login</button>
+			</form>
+
+		</div>
+	);
 };
 
 export default Login;
